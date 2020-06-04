6de9d0e1360e237fc74a9051d1c7350b7d0ba40d
MaleneBille <43337016+MaleneBille@users.noreply.github.com>
2020-06-04 10:37:18 +0200

    Update article.php

--- article.php
+++ article.php
@@ -22,30 +22,30 @@
                         <br>1)  Gå til Sitecore content editor
                                 - Lederne - Forside - Presse & Nyheder - Debat.
                         <br>
-                        <img class="responsive_image" src="images/artikel/opret_artikel2.PNG" alt="step 1">
+                        <img class="responsive_image" src="images/artikel/opret_artikel2.png" alt="step 1">
                         <br>
                         <br>2)  Højreklik på "Debat" og vælg "Article".
                         <br>
-                        <img class="responsive_image" src="images/artikel/opret_artikel3.PNG" alt="step 1">
+                        <img class="responsive_image" src="images/artikel/opret_artikel3.png" alt="step 1">
                         <br>
                         <br>3)   Herefter indtaster du artiklens titel i pop-up vinduet.
                         <br>
-                        <img class="responsive_image" src="images/artikel/opret_artikel4.PNG" alt="step 1">
+                        <img class="responsive_image" src="images/artikel/opret_artikel4.png" alt="step 1">
                         <br>
                         <br>
                         <br>4)  Din artikel er oprettet, og titlen fremgår to steder på siden.
                         <br>
-                        <img class="responsive_image" src="images/artikel/opret_artikel5.PNG" alt="step 1">
+                        <img class="responsive_image" src="images/artikel/opret_artikel5.png" alt="step 1">
                         <br>
                         <br>
                         <br>5)  Scroll ned til navigation og dobbeltklik på de tags du skal bruge. Fjern herefter vingen i Navigation - Show in navigation
                         <br>
-                        <img class="responsive_image" src="images/artikel/opret_artikel6.PNG" alt="step 1">
+                        <img class="responsive_image" src="images/artikel/opret_artikel6.png" alt="step 1">
                         <br>
                         <br>
                         <br>6)  Gem artiklen
                         <br>
-                        <img class="responsive_image" src="images/artikel/opret_artikel7.PNG" alt="step 1">
+                        <img class="responsive_image" src="images/artikel/opret_artikel7.png" alt="step 1">
                         <br>Du er nu klar til næste step, som er at sætte artiklen ind i experience editoren.
                     </div>
                 </div>
